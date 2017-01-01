32a08bff5f8d5ee59de3b9216a4e795835fdf9e9
khalim19 <khalim19@gmail.com>
2017-01-01 23:22:54 +0100

    PDB procedures: Convert matching integer parameters to booleans
    "Matching integer parameters" are those containing substrings like "(TRUE or FALSE)", indicating their usage as booleans. These parameters are now assigned the `bool` type and the matching substring is removed most of the time.

--- pypredef_generator_pdb.py
+++ pypredef_generator_pdb.py
@@ -8,6 +8,7 @@ procedural database (PDB).
 from __future__ import absolute_import, print_function, division, unicode_literals
 
 import inspect
+import re
 
 import ast
 
@@ -91,6 +92,67 @@ _PDB_TYPES_MAP = {type_item[0]: PdbType(*type_item) for type_item in _PDB_TYPE_I
 #===============================================================================
 
 
+class PdbParam(object):
+  
+  def __init__(self, pdb_type_id, name, description=None):
+    self._pdb_type_id = pdb_type_id
+    self._orig_name = name.decode(pypredef_generator.TEXT_FILE_ENCODING)
+    
+    self.description = (
+      description.decode(pypredef_generator.TEXT_FILE_ENCODING) if description is not None else "")
+    self.pdb_type = PdbType.get_by_id(pdb_type_id)
+    self.name = self._get_param_name(self._orig_name)
+  
+  @property
+  def pdb_type_id(self):
+    return self._pdb_type_id
+  
+  @property
+  def orig_name(self):
+    return self._orig_name
+  
+  @classmethod
+  def _get_param_name(cls, orig_name):
+    return orig_name.replace("-", "_")
+
+
+def get_pdb_params(pdb_function):
+  """
+  Return PDB function parameters and a boolean indicating whether `run_mode`
+  parameter is in the parameter list.
+  
+  If `run_mode` in the parameter list, it is guaranteed to be at the end of the
+  parameter list.
+  """
+  
+  pdb_params = [PdbParam(*pdb_param_info) for pdb_param_info in pdb_function.params]
+  has_run_mode_param = _move_run_mode_param_to_end(pdb_params)
+  
+  return pdb_params, has_run_mode_param
+
+
+def _move_run_mode_param_to_end(pdb_params):
+  run_mode_param_index = _get_run_mode_param_index(pdb_params)
+  has_run_mode_param = run_mode_param_index is not None
+  
+  if has_run_mode_param:
+    run_mode_param = pdb_params.pop(run_mode_param_index)
+    pdb_params.append(run_mode_param)
+  
+  return has_run_mode_param
+
+
+def _get_run_mode_param_index(pdb_params):
+  for pdb_param_index, pdb_param in enumerate(pdb_params):
+    if pdb_param.name == "run_mode":
+      return pdb_param_index
+  
+  return None
+
+
+#===============================================================================
+
+
 def generate_predefined_completions_for_gimp_pdb():
   node_pdb = pypredef_generator.get_ast_node_for_root_module(gimp.pdb)
   pypredef_generator.insert_ast_docstring(node_pdb, gimp.pdb)
@@ -104,6 +166,10 @@ def generate_predefined_completions_for_gimp_pdb():
       pypredef_generator.insert_ast_node(pdb_member_name, gimp.pdb, node_pdb)
   
   pypredef_generator.write_pypredef_file("gimp.pdb", node_pdb)
+
+
+def _is_member_pdb_function(member):
+  return type(member).__name__ == "PDBFunction"
 
 
 def _insert_ast_node_for_pdb_function(pdb_function_name, pdb_function, node_pdb):
@@ -126,25 +192,15 @@ def _get_ast_arguments_for_pdb_function(pdb_function):
   args = []
   defaults = []
   
-  pdb_params, has_run_mode_param = _get_pdb_params(pdb_function)
+  pdb_params, has_run_mode_param = get_pdb_params(pdb_function)
   
   if has_run_mode_param:
-    pdb_params = pdb_params[:-1]
+    defaults.append(ast.Name(id="gimpenums.RUN_NONINTERACTIVE"))
   
-  for pdb_param_info in pdb_params:
-    pdb_param_name = _get_pdb_param_name(pdb_param_info)
-    args.append(pdb_param_name)
-  
-  if has_run_mode_param:
-    run_mode_name, run_mode_default_value_name = _get_run_mode_parameter_name_and_default_value()
-    args.append(ast.Name(id=run_mode_name))
-    defaults.append(ast.Name(id=run_mode_default_value_name))
+  for pdb_param in pdb_params:
+    args.append(pdb_param.name)
   
   return ast.arguments(args=args, vararg=None, kwarg=None, defaults=defaults)
-
-
-def _get_run_mode_parameter_name_and_default_value():
-  return "run_mode", "gimpenums.RUN_NONINTERACTIVE"
 
 
 def _get_ast_docstring_for_pdb_function(pdb_function):
@@ -164,23 +220,52 @@ def _get_ast_docstring_for_pdb_function(pdb_function):
 
 
 def _get_pdb_docstring_param_info(pdb_function):
-  docstring_param_info = ""
-  pdb_params = _get_pdb_params(pdb_function)[0]
+  params_docstring = ""
+  pdb_params, unused_ = get_pdb_params(pdb_function)
   
   if pdb_params:
-    docstring_param_info += "Parameters:\n"
-    
-    for pdb_param_info in pdb_params:
-      docstring_param_info += _get_pdb_param_docstring(pdb_param_info) + "\n"
+    params_docstring += "Parameters:\n"
+    for pdb_param in pdb_params:
+      _convert_int_pdb_param_to_bool(pdb_param)
+      params_docstring += _get_pdb_params_docstring(pdb_param) + "\n"
   
-  return docstring_param_info
+  return params_docstring
 
 
-def _get_pdb_param_docstring(pdb_param_info):
+def _convert_int_pdb_param_to_bool(pdb_param):
+  bool_param_description_true_false_regex_format = (
+    r"[\.:]? *\(?{0}(  *or  *| *[/,] *){1}\)?"
+    r"|[\.:]? *\{{ *{0}( *\({2}\))?, *{1}( *\({3}\))? *\}}")
+  
+  pdb_bool_param_description_match_regex_components = (
+    bool_param_description_true_false_regex_format.format(r"true", r"false", r"1", r"0")
+    + r"|"
+    + bool_param_description_true_false_regex_format.format(r"false", r"true", r"0", r"1"))
+  
+  pdb_bool_param_description_match_regex = (
+    r"(" + pdb_bool_param_description_match_regex_components + r"|\?$" + r")")
+  
+  is_pdb_param_bool = (
+    pdb_param.pdb_type.type_ is int
+    and re.search(
+      pdb_bool_param_description_match_regex,
+      pdb_param.description, flags=re.UNICODE | re.IGNORECASE))
+  
+  if is_pdb_param_bool:
+    pdb_bool_param_description_substitute_regex = (
+      r"(" + pdb_bool_param_description_match_regex_components + r")$")
+    
+    pdb_param.pdb_type = PdbType(pdb_param.pdb_type.type_, bool)
+    pdb_param.description = re.sub(
+      pdb_bool_param_description_substitute_regex, "",
+      pdb_param.description, flags=re.UNICODE | re.IGNORECASE)
+
+
+def _get_pdb_params_docstring(pdb_param):
   return "{0} ({1}): {2}".format(
-    _get_pdb_param_name(pdb_param_info),
-    PdbType.get_by_id(pdb_param_info[0]).get_name(include_base_type=True),
-    _get_pdb_param_description(pdb_param_info))
+    pdb_param.name,
+    pdb_param.pdb_type.get_name(include_base_type=True),
+    pdb_param.description)
 
 
 def _get_ast_return_value_types_for_pdb_function(pdb_function):
@@ -195,41 +280,3 @@ def _get_ast_return_value_types_for_pdb_function(pdb_function):
     node_return_value_types = ast.Name(id="None")
   
   return ast.Return(value=node_return_value_types)
-
-
-def _is_member_pdb_function(member):
-  return type(member).__name__ == "PDBFunction"
-
-
-def _get_pdb_params(pdb_function):
-  """
-  Return PDB function parameters and a boolean indicating whether `run_mode`
-  parameter is in the parameter list.
-  
-  If `run_mode` in the parameter list, it is guaranteed to be at the end of the
-  parameter list.
-  """
-  
-  pdb_params = []
-  run_mode_param_info = None
-  
-  for pdb_param_info in pdb_function.params:
-    if _get_pdb_param_name(pdb_param_info) == "run_mode":
-      run_mode_param_info = pdb_param_info
-    else:
-      pdb_params.append(pdb_param_info)
-  
-  if run_mode_param_info is not None:
-    pdb_params.append(run_mode_param_info)
-  
-  has_run_mode_param = run_mode_param_info is not None
-  
-  return pdb_params, has_run_mode_param
-
-
-def _get_pdb_param_name(pdb_param_info):
-  return pdb_param_info[1].replace("-", "_")
-
-
-def _get_pdb_param_description(pdb_param_info):
-  return pdb_param_info[2] if pdb_param_info[2] is not None else ""
